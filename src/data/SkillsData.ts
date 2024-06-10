c1a8bc6bc7475c247e811ae119509f6763d6a3ba
Warre Welvaert <warrewelvaert@gmail.com>
2024-06-10 19:23:43 +0200

    Fix icon paths

--- src/data/SkillsData.ts
+++ src/data/SkillsData.ts
@@ -4,27 +4,27 @@ const SkillsData = [
     skills: [
       {
         name: 'JavaScript',
-        icon: '../../public/assets/icons/language-javascript.svg',
+        icon: '/assets/icons/language-javascript.svg',
         link: 'https://developer.mozilla.org/en-US/docs/Web/JavaScript',
       },
       {
         name: 'Java',
-        icon: '../../public/assets/icons/language-java.svg',
+        icon: '/assets/icons/language-java.svg',
         link: 'https://www.oracle.com/java/',
       },
       {
         name: 'Python',
-        icon: '../../public/assets/icons/language-python.svg',
+        icon: '/assets/icons/language-python.svg',
         link: 'https://www.python.org/',
       },
       {
         name: 'C#',
-        icon: '../../public/assets/icons/language-csharp.svg',
+        icon: '/assets/icons/language-csharp.svg',
         link: 'https://learn.microsoft.com/en-us/dotnet/csharp/',
       },
       {
         name: 'C++',
-        icon: '../../public/assets/icons/language-cpp.svg',
+        icon: '/assets/icons/language-cpp.svg',
         link: 'https://isocpp.org/',
       },
     ],
@@ -34,22 +34,22 @@ const SkillsData = [
     skills: [
       {
         name: 'Django',
-        icon: '../../public/assets/icons/django.svg',
+        icon: '/assets/icons/django.svg',
         link: 'https://www.djangoproject.com/',
       },
       {
         name: 'ASP .NET',
-        icon: '../../public/assets/icons/dot-net.svg',
+        icon: '/assets/icons/dot-net.svg',
         link: 'https://dotnet.microsoft.com/apps/aspnet',
       },
       {
         name: 'Vue.js',
-        icon: '../../public/assets/icons/vuejs.svg',
+        icon: '/assets/icons/vuejs.svg',
         link: 'https://vuejs.org/',
       },
       {
         name: 'Nuxt',
-        icon: '../../public/assets/icons/nuxt.svg',
+        icon: '/assets/icons/nuxt.svg',
         link: 'https://nuxtjs.org/',
       },
     ],
@@ -59,17 +59,17 @@ const SkillsData = [
     skills: [
       {
         name: 'MySQL',
-        icon: '../../public/assets/icons/mysql.svg',
+        icon: '/assets/icons/mysql.svg',
         link: 'https://www.mysql.com/',
       },
       {
         name: 'MongoDB',
-        icon: '../../public/assets/icons/mongodb.svg',
+        icon: '/assets/icons/mongodb.svg',
         link: 'https://www.mongodb.com/',
       },
       {
         name: 'PostgreSQL',
-        icon: '../../public/assets/icons/psql.svg',
+        icon: '/assets/icons/psql.svg',
         link: 'https://www.postgresql.org/',
       },
     ],
@@ -79,27 +79,27 @@ const SkillsData = [
     skills: [
       {
         name: 'HTML',
-        icon: '../../public/assets/icons/language-html5.svg',
+        icon: '/assets/icons/language-html5.svg',
         link: 'https://developer.mozilla.org/en-US/docs/Web/HTML',
       },
       {
         name: 'CSS',
-        icon: '../../public/assets/icons/language-css3.svg',
+        icon: '/assets/icons/language-css3.svg',
         link: 'https://developer.mozilla.org/en-US/docs/Web/CSS',
       },
       {
         name: 'SASS',
-        icon: '../../public/assets/icons/sass.svg',
+        icon: '/assets/icons/sass.svg',
         link: 'https://sass-lang.com/',
       },
       {
         name: 'Node.js',
-        icon: '../../public/assets/icons/nodejs.svg',
+        icon: '/assets/icons/nodejs.svg',
         link: 'https://nodejs.org/',
       },
       {
         name: 'Tailwind CSS',
-        icon: '../../public/assets/icons/tailwind.svg',
+        icon: '/assets/icons/tailwind.svg',
         link: 'https://tailwindcss.com/',
       },
     ],
@@ -109,17 +109,17 @@ const SkillsData = [
     skills: [
       {
         name: 'Git',
-        icon: '../../public/assets/icons/git.svg',
+        icon: '/assets/icons/git.svg',
         link: 'https://git-scm.com/',
       },
       {
         name: 'GitHub',
-        icon: '../../public/assets/icons/github.svg',
+        icon: '/assets/icons/github.svg',
         link: 'https://github.com/',
       },
       {
         name: 'GitLab',
-        icon: '../../public/assets/icons/gitlab.svg',
+        icon: '/assets/icons/gitlab.svg',
         link: 'https://docs.gitlab.com/',
       },
     ],
@@ -129,17 +129,17 @@ const SkillsData = [
     skills: [
       {
         name: 'Docker',
-        icon: '../../public/assets/icons/docker.svg',
+        icon: '/assets/icons/docker.svg',
         link: 'https://www.docker.com/',
       },
       {
         name: 'Firebase',
-        icon: '../../public/assets/icons/firebase.svg',
+        icon: '/assets/icons/firebase.svg',
         link: 'https://firebase.google.com/',
       },
       {
         name: 'GCP',
-        icon: '../../public/assets/icons/google-cloud.svg',
+        icon: '/assets/icons/google-cloud.svg',
         link: 'https://cloud.google.com/',
       },
     ],
